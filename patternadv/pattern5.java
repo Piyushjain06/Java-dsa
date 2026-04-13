36785bd10439d7bc8a5f814ff80d31eb563d49eb
Piyushjain06 <piyushjainji@outlook.com>
2026-04-13 21:34:49 +0530

    pattern done

---
+++ patternadv/pattern5.java
@@ -0,0 +1,21 @@
+package patternadv;
+
+public class pattern5 {
+    //0-1 triangle
+        public static void main(String args[]){
+            int n=5;
+            for (int i=1;i<=5;i++ ){
+                for (int j=1;j<=i;j++){
+                    if ((j+i)%2==0){
+                        System.out.print("1 ");
+                    }
+                    else{
+                        System.out.print("0 ");
+                    }
+                }
+                System.out.println();
+            }
+
+        }
+
+}
